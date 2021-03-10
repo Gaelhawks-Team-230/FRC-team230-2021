9ac4e0fc8685f5ebdafa2548a87c63b6e4890ce5
Gaelhawks-Programming <80365709+Gaelhawks-Programming@users.noreply.github.com>
2021-03-09 20:23:08 -0500

    Project Rebase

---
+++ src/main/include/LightController.h
@@ -0,0 +1,32 @@
+#ifndef LIGHTCONTROLLER_H_
+#define LIGHTCONTROLLER_H_
+
+#include "Common.h"
+#include <frc/DigitalOutput.h>
+
+class TalonXXI;
+
+
+class LightController
+{
+    private:
+  
+        frc::DigitalOutput *light1;
+        frc::DigitalOutput *light2;
+        frc::DigitalOutput *light3;
+
+        TalonXXI *mainRobot;
+        
+        bool isTargetting;
+        int loopCount;
+   
+    public:
+        LightController(TalonXXI* pRobot);
+        void LocalReset(void);
+        void StartingConfig(void);
+        void StopAll(void);
+        void UpdateDash(void);
+        void Service(void);
+
+};
+#endif /*LIGHTCONTROLLER_H_*/
