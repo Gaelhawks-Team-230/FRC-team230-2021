169671710e9713a63b818695d4ea96261519fd1d
Gaelhawks-Programming <80365709+Gaelhawks-Programming@users.noreply.github.com>
2021-03-09 20:12:00 -0500

    Rebase

---
+++ cpp/Climber.cpp
@@ -0,0 +1,139 @@
+#include "Common.h"
+#include "Climber.h"
+
+Climber::Climber(TalonXXI* pRobot)
+{
+    mainRobot = pRobot;
+    climbExtendMotor = new frc::VictorSP(PWM_CLIMB_EXTEND);
+    climbWinchMotor = new frc::VictorSP(PWM_CLIMB_WINCH);
+    localSurveillance = mainRobot->surveillance;
+    
+    LocalReset();
+}
+
+//Sets all local variables
+void Climber::LocalReset()
+{
+    extendPos = localSurveillance->GetClimbExtendDis();
+    goalPos = extendPos;
+    winchPos = localSurveillance->GetClimbWinchDis();
+    extendMotorCmd = 0.0;
+    winchMotorCmd = 0.0;
+    isClimbing = false;
+    isManualMove = false;
+    loopCount = 0;
+}
+
+//Holds how the variables should be set when the robot starts
+void Climber::StartingConfig()
+{
+
+}
+
+//Deploys solenoid piston
+void Climber::DeployHook()
+{
+}
+
+//Turns climb motor off
+void Climber::StopClimb()
+{
+   // motorCmd= 0.0;
+    //mode= IDLE; ???
+}
+
+//Checks if robot is climbing
+bool Climber::IsClimbing()
+{
+    if(extendPos > IS_CLIMBING_THRESHOLD)
+    {
+        isClimbing = true;
+    }
+    return isClimbing;
+}
+
+void Climber::StopAll()
+{
+    LocalReset();
+}
+
+void Climber::UpdateDash()
+{
+    //frc::SmartDashboard::PutNumber("Climb Extend Goalpos: ", goalPos);
+    //frc::SmartDashboard::PutNumber("Climb Extend Cmd: ", extendMotorCmd);
+    //frc::SmartDashboard::PutNumber("Climb Winch Cmd: ", winchMotorCmd);
+}
+
+void Climber::Service()
+{
+    loopCount++;
+    extendPos = localSurveillance->GetClimbExtendDis();
+    winchPos = localSurveillance->GetClimbWinchDis();
+#ifndef TEST_MODE
+   IsClimbing();
+   //extendErr = goalPos - extendPos;
+ //  extendMotorCmd = extendErr * CLIMBER_EXTEND_K;
+   extendMotorCmd = TalonXXI::Limit(-CLIMBER_MAX_CMD, CLIMBER_MAX_CMD, extendMotorCmd);
+#endif
+    goalPos = TalonXXI::Limit(MIN_CLIMB_EXTEND_DIS, MAX_CLIMB_EXTEND_DIS, goalPos);
+  //  printf("%d %f %f %f %f %f \n", loopCount, extendMotorCmd, extendPos, goalPos, winchMotorCmd, winchPos);
+    climbExtendMotor->Set(extendMotorCmd);
+    climbWinchMotor->Set(winchMotorCmd);
+}
+
+void Climber::MoveExtender(double input)
+{
+    /*if(input != 0.0 && !isManualMove)
+    {
+        goalPos = extendPos;
+        isManualMove = true;
+    }*/
+    if(input != 0.0)
+    {
+        //goalPos = goalPos + TalonXXI::Sign(input)*CLIMBER_MANUAL_DELTA;
+        extendMotorCmd = TalonXXI::Sign(input)*CLIMBER_MAX_CMD;
+    }
+    else
+    {
+        extendMotorCmd = 0.0;
+    }
+}
+
+void Climber::MoveWinch(double input)
+{
+    winchMotorCmd = input;
+}
+
+void Climber::GiveGoalPos(double inGoalPos)
+{
+    goalPos = inGoalPos;
+    isManualMove = false;
+}
+
+#ifdef TEST_MODE
+void Climber::ExtendTesting(double input)
+{
+    if(fabs(input) > 0.1)
+    {
+        extendMotorCmd = input;
+    }
+    else
+    {
+        extendMotorCmd = 0.0;
+    }
+    extendMotorCmd = TalonXXI::Limit(-0.6, 0.6, extendMotorCmd);
+}
+
+void Climber::WinchTesting(double input)
+{
+    if(fabs(input) > 0.1)
+    {
+        winchMotorCmd = input;
+    }
+    else
+    {
+        winchMotorCmd = 0.0;
+    }
+    winchMotorCmd = TalonXXI::Limit(-0.6, 0.6, winchMotorCmd);
+}
+#endif
---
+++ cpp/LightController.cpp
@@ -0,0 +1,105 @@
+#include "Common.h"
+#include "LightController.h"
+
+LightController::LightController(TalonXXI* pRobot)
+{
+    light1 = new frc::DigitalOutput(LED_LIGHT_OUTPUT_3);
+	light2 = new frc::DigitalOutput(LED_LIGHT_OUTPUT_2);
+	light3 = new frc::DigitalOutput(LED_LIGHT_OUTPUT_1);
+    mainRobot = pRobot;
+    LocalReset();
+}
+
+void LightController::LocalReset()
+{
+    light1->Set(true);
+    light2->Set(true);        
+    light3->Set(true);
+    isTargetting = false;
+    loopCount = 0;
+}
+
+void LightController::StartingConfig()
+{
+    LocalReset();
+}
+
+void LightController::StopAll()
+{
+    LocalReset();
+}
+
+
+//Anything that you want printed on the dashboard for testing or during match. 
+//Any information which may be important. 
+void LightController::UpdateDash()
+{
+
+}
+//Called every loop
+void LightController::Service()
+{
+    //int cellCount = mainRobot->deathStar->GetCellCount();
+    light1->Set(true);
+    light2->Set(true);
+    light3->Set(true);
+    /*
+    loopCount++;
+    cellCount = (int) (loopCount/100);
+    printf("cellCount %d\n", cellCount);
+    if (cellCount > 5)
+    {
+        cellCount = 0;
+        loopCount = 0;
+    }
+
+    if(cellCount == 0)
+    {
+        //Search mode :)
+        //good
+        light1->Set(true);
+        light2->Set(true);
+        light3->Set(true);
+    }
+    else if(cellCount == 1)
+    {
+        //good
+        light1->Set(true);
+        light2->Set(true);
+        light3->Set(false);
+    }
+    else if(cellCount == 2)
+    {
+        
+        //good
+        light1->Set(true);
+        light2->Set(false);
+        light3->Set(true);
+    }
+    else if(cellCount == 3)
+    {
+        light1->Set(true);
+        light2->Set(false);
+        light3->Set(false);
+    }
+    else if(cellCount == 4)
+    {
+        light1->Set(false);
+        light2->Set(true);
+        light3->Set(true);
+    }
+    else if(cellCount == 5)
+    {
+        light1->Set(false);
+        light2->Set(true);
+        light3->Set(false);
+
+    }
+    else
+    {
+        light1->Set(false);
+        light2->Set(false);
+        light3->Set(false);
+    }
+    */
+} 
